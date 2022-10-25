0f4da86a036ce0abecf774af4331e0e1f50f59a8
Kovie <40118781+thekovie@users.noreply.github.com>
2022-10-25 21:32:33 +0800

    Add Functions in Player and Tile

--- src/models/Player.java
+++ src/models/Player.java
@@ -6,28 +6,26 @@ public class Player {
     private int level;
     private FarmerType type;
 
-    public Player()
-    {
-
+    public Player() {
+        this.coins = 0;
+        this.exp = 0;
+        this.level = 0;
+        //this.type = 
     }
 
-    public int getCoins()
-    {
+    public int getCoins() {
         return this.coins;
     }
 
-    public float getExp()
-    {
+    public float getExp() {
         return this.exp;
     }
 
-    public int getLevel()
-    {
+    public int getLevel() {
         return this.level;
     }
 
-    public FarmerType getType()
-    {
+    public FarmerType getType() {
         return this.type;
     }
 
@@ -36,27 +34,31 @@ public class Player {
      *
      * @returns whether the player leveled up or not.
      */
-    public boolean addExp(float num)
-    {
+    public boolean addExp(float num) {
+        if (this.exp + num > 1000)
+            return false;
+        
+        this.exp += num;
         return true;
     }
 
     /**
      * @returns whether the player's coins was successfully decreased or not
      */
-    public boolean decCoins(int num)
-    {
+    public boolean decCoins(int num) {
+        if (this.coins - num < 0)
+            return false;
+
+        this.coins -= num;
         return true;
     }
 
-    public void incCoins(int num)
-    {
-
+    public void incCoins(int num) {
+        this.coins += num;
     }
 
-    public void setType(FarmerType type)
-    {
-
+    public void setType(FarmerType type) {
+        this.type = type;
     }
 
 }
